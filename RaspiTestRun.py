0eded2c8e6e995fd7b07389f7f1e468c78c26341
Lucas Wiseby <l.wiseby@gmail.com>
2019-04-20 21:16:25 +0200

    "debugging"

--- RaspiTestRun.py
+++ RaspiTestRun.py
@@ -34,10 +34,10 @@ log = ''
 logFile = 'test.log'
 gitPull = 'git pull origin master'
 
-if len(sys.argv) > 0:
+if len(sys.argv) > 1:
     testProgram = str(sys.argv[1])
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
     remoteRepo = sys.argv[2]
 
 
