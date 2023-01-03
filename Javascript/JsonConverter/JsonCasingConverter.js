7eb54ce8335f827cb9078c37a325eb7db577c569
Lucas Wiseby <lucas.wiseby@maksimer.com>
2023-01-03 16:20:55 +0100

    Add support for PascalCase conversion

--- Javascript/JsonConverter/JsonCasingConverter.js
+++ Javascript/JsonConverter/JsonCasingConverter.js
@@ -1,23 +1,23 @@
-// Converts all json object keys to camelCase
+// Converts all json object keys to camelCase or PascalCase
 // Code for toCamel method is taken from this thread:
 // https://exceptionshub.com/convert-returned-json-object-properties-to-lower-first-camelcase.html
 // Could have made this my self but this saved me some time. Thank you admin at Exceptionshub.com!
 // Usage:
-// node JsonCamelCaseConverter([Json file to convert], [result destination])
+// node JsonCamelCaseConverter([Json file to convert], [result destination], [convert to pascalcase true/false default false])
 // If no destination is specified it will write result.json to script location.
 
 const fs = require('fs');
 
 const args = process.argv;
 
-const toCamel = (o) => {
+const transform = (o, p) => {
   var newO, origKey, newKey, value;
   if (o instanceof Array) {
     newO = [];
     for (origKey in o) {
       value = o[origKey];
       if (typeof value === 'object') {
-        value = toCamel(value);
+        value = transform(value, p);
       }
       newO.push(value);
     }
@@ -25,10 +25,12 @@ const toCamel = (o) => {
     newO = {};
     for (origKey in o) {
       if (o.hasOwnProperty(origKey)) {
-        newKey = (origKey.charAt(0).toLowerCase() + origKey.slice(1) || origKey).toString();
+        const theWay = p === false ? () => origKey.charAt(0).toLowerCase()
+                                   : () => origKey.charAt(0).toUpperCase();
+        newKey = (theWay() + origKey.slice(1) || origKey).toString();
         value = o[origKey];
         if (value instanceof Array || (value !== null && value.constructor === Object)) {
-          value = toCamel(value);
+          value = transform(value, p);
         }
         newO[newKey] = value;
       }
@@ -37,10 +39,10 @@ const toCamel = (o) => {
   return newO;
 };
 
-function convert(src, dest) {
+function convert(src, dest, pascalCase) {
   let rawData = fs.readFileSync(src);
   let items = JSON.parse(rawData);
-  let convertedItems = toCamel(items)
+  let convertedItems = transform(items, pascalCase)
   if (dest == undefined) {
     fs.writeFileSync('./result.json', JSON.stringify(convertedItems));
   } else {
@@ -49,4 +51,4 @@ function convert(src, dest) {
   }
 }
 
-convert(args[2], args[3]);
+convert(args[2], args[3], args[4]);
